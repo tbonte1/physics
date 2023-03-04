58f1fecd80b0ae9f8f73d71f8586da7656946840
tbonte1 <119233068+tbonte1@users.noreply.github.com>
2023-03-04 14:06:54 -0500

    Add files via upload

---
+++ ruler.js
@@ -0,0 +1,5 @@
+app.addEventListener('input', (e) => {
+    const input = e.target;
+    const value = input.type === 'checkbox' ? (input.checked ? 1 : 0) : input.value;
+    document.body.style.setProperty(input.name, value+(input.dataset.suffix||''));
+  })
